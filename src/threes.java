8e72651d11785cf125d1c00d95d73e880fcf4a64
Ed-Home <bowenjuice@gmail.com>
2014-05-20 15:32:13 +1200

    Starting Set uP

--- src/threes.java
+++ src/threes.java
@@ -1,5 +1,35 @@
 /**
  * Created by tae on 5/20/14.
+ *
+ * x^2 = y^2 + z^2  THEREFORE UPPER BOUND ON EITHER Y or Z = X-1
+ * x y z are positive integers
+ * x y z have no factors in common
+ * x is divisible by 5
+ *
  */
+
 public class threes {
+    private static int count = 0, y = 1, z = 1, x = 5;
+
+    /**
+     * List the first 350 sets of three in increasing order of x
+     */
+    public static void question1(){
+        while (count < 350){ // List the first 350
+
+            while(y<x){
+
+            }
+
+
+
+
+
+        }
+    }
+
+
+    public static void main(String[] args){
+        question1();
+    }
 }
