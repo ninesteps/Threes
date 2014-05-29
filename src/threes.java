f7407646bdf0c632e230be47f6e973c0c71de69c
Ed-Home <bowenjuice@gmail.com>
2014-05-29 20:07:50 +1200

    Fixed overflow issues

--- src/threes.java
+++ src/threes.java
@@ -1,26 +1,29 @@
 public class threes {
 
 
-    private static int gcd(int a, int b) {
+    private static long gcd(long a, long b) {
         return (b==0) ? a : gcd(b,a%b);
     }
-    private static int gcd(int a, int b, int c) {
+    private static long gcd(long a, long b, long c) {
         return gcd(gcd(a,b),c);
     }
 
     private static void smallestZ() {
         System.out.println("First 350 Sets in order of Smallest Z");
         int count = 0;
-        int z = 1;
-        int y = 2;
+        long z = 1;
 
         while(count<350){
-            int x = (int)Math.sqrt(z*z + y*y);
+            for(long y = 2; y<200000; y++) {
+            long x = (int)Math.sqrt(z*z + y*y);
+
             if(x*x == z*z + y*y && x%5==0 && gcd(z,y,x)==1) {
                 System.out.println(x + " " + y + " " + z);
                 count++;
             }
             y++;
+        }
+            z++;
         }
     }
 
@@ -28,8 +31,9 @@ public class threes {
         System.out.println("First 350 Sets in order of Unique Even Y");
         int count =0;
 
-        for(int y=4;count<140;y+=2) {
-            int num = (int) Math.pow((y/2),2);
+        for (int y2 = 2; count < 140; y2++) {
+            int y = y2*2;
+            int num = y2*y2;
             if((y*y + (num-1)*(num-1)) == (num+1)*(num+1) && ((num+1)%5==0) && gcd(y,num-1,num+1)==1) {
                 System.out.println((num+1) + " " + y + " " + (num-1));
                 count++;
@@ -41,12 +45,12 @@ public class threes {
     private static void increasingX() {
         System.out.println("First 350 Sets in increasing order of X");
         int count = 0;
-        int y;
-        int x = 5;
+        long y;
+        long x = 5;
 
         while(count<350){
-            for(int z = 1; z<20000; z++) {
-                y = (int) Math.sqrt(1+ (x*x - z*z));
+            for(long z = 1; z<x; z++) {
+                y = (int) Math.sqrt(x*x - z*z);
                 if((y*y + z*z) == x*x && z<y && gcd(x,y,z)==1) {
                     System.out.println(x + " " + y + " " + z);
                     count++;
